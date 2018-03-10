37223de960e190a3adf9d42f98e596acfac7c654
Habib <jhabib@amazon.com>
2018-03-10 12:15:13 -0800

    added long2 file to include all years; included header; add SNOW column to represent SNOWFALL that day;

--- data/weather/combined/reshape_weather_data.R
+++ data/weather/combined/reshape_weather_data.R
@@ -1,17 +1,22 @@
 library(dplyr)
 library(reshape2)
 
-setwd('~/Documents/ucb-mids/w209project-s2018/data/weather/combined/')
+setwd('~/Documents/ucb-mids/w209project-s2018/data/weather/combined/CombinedArchive/')
 
 
 
 cols.to.keep <- c('FIRE_ID', 'OBS_DATE', 'START_DATE', 'END_DATE', 'DAYS', 
                   'FIRE_LAT', 'FIRE_LON', 'FIRE_YEAR', 'STATION_ID', 
                   'STAT_LAT', 'STAT_LON', 'DIST', 'BEARING', 
-                  'TAVG', 'TMAX', 'TMIN', 'PRCP', 'SNWD')
+                  'TAVG', 'TMAX', 'TMIN', 'PRCP', 'SNWD', 'SNOW')
 
+new.header <- c('FIRE_ID', 'OBS_DATE', 'START_DATE', 'END_DATE', 'DAYS', 
+                  'FIRE_LAT', 'FIRE_LON', 'FIRE_YEAR', 'STATION_ID', 
+                  'STAT_LAT', 'STAT_LON', 'DIST', 'BEARING', 
+                  'TAVG', 'TMAX', 'TMIN', 'PRCP', 'SNWD', 'SNOW')
 
-write.table(rbind(cols.to.keep), file = './long/headers.csv', sep = ',', row.names = FALSE, col.names = FALSE)
+# write.table(rbind(cols.to.keep), file = './long/headers.csv', sep = ',', row.names = FALSE, col.names = FALSE)
+write.table(rbind(cols.to.keep), file = '../long2/headers.csv', sep = ',', row.names = FALSE, col.names = FALSE)
 
 ConvertToLongFormat <- function(fn, cols) {
   d <- read.csv(paste(fn, '_combined.csv', sep = ''))
@@ -19,9 +24,11 @@ ConvertToLongFormat <- function(fn, cols) {
   head(d.l)
   d.s <- subset(d.l, select = cols)
   colnames(d.s) <- cols
-  # cat(colnames(d.s))
+  d.s$TAVG <- d.s$TAVG / 10.0
+  d.s$TMIN <- d.s$TMIN / 10.0
+  d.s$TMAX <- d.s$TMAX / 10.0
   d.s$OBS_DATE <- as.Date(as.character(d.s$OBS_DATE), '%Y%m%d')
-  write.table(d.s, sep=',', file = paste('./long/all_years_long.csv', sep = ''), append = TRUE, row.names = FALSE, col.names = FALSE)
+  write.table(d.s, sep=',', file = paste('../long2/all_years_long2.csv', sep = ''), append = TRUE, row.names = FALSE, col.names = FALSE)
 }
 
 # ConvertToLongFormat(2015, cols.to.keep)
@@ -36,6 +43,12 @@ ConvertToLongFormat <- function(fn, cols) {
 for (year in c(1997:2015)) {
   ConvertToLongFormat(year, cols.to.keep)
 }
+
+# Add the header back from bash
+# echo -e "$(cat headers.csv)\n$(cat all_years_long2.csv)" > all_years_long2.csv 
+
+# d.n <- read.csv('../long2/all_years_long2.csv', header = FALSE)
+# colnames(d.n) <- new.header
 
 # Bash, combine all csvs into one
 # paste -d , *_long.csv > all_years_long.csv
